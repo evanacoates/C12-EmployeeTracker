88f8066b3c11bf8f377bc1124bd275cec4b6d5d6
Evan <evanacoates@gmail.com>
2023-04-24 10:21:33 -0400

    role table

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,10 @@ CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY, 
     department_name VARCHAR(30) NOT NULL);
 
-CREATE TABLE roles();
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL);
 
 CREATE TABLE employee();
