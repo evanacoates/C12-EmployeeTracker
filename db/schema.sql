a5c225ce14617fecf910d378b8249565912d1e48
Evan <evanacoates@gmail.com>
2023-04-24 10:18:54 -0400

    department table

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,9 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department();
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    department_name VARCHAR(30) NOT NULL);
 
 CREATE TABLE roles();
 
