c70311e5411ba6614b127d5265b89c478bf597c1
Evan <evanacoates@gmail.com>
2023-04-24 10:24:49 -0400

    employee table

--- db/schema.sql
+++ db/schema.sql
@@ -14,4 +14,9 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL);
 
-CREATE TABLE employee();
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL);
+    
