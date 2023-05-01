b739e490591e90edf011939ccd2bf6e8c308b039
Evan <evanacoates@gmail.com>
2023-05-01 12:17:34 -0400

    index.js db connection

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
-const db = mysql.createConnection();
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'employeeTracker_db',
+    },
+    console.log(`Connected to the db.`)
+  );
